0f8bc1d99036ce0f29f8b15128960e959d3ac2e0
Gustavo Morini <gustavommh@gmail.com>
2023-02-03 17:45:42 -0300

    base project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+import SignIn from "./src/pages/SignIn";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        backgroundColor="#1d1d2e"
+        barStyle="light-content"
+        translucent={false}
+      />
+      <SignIn />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#1d1d2e",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SignIn() {
+  return (
+    <View style={{ paddingHorizontal: 14 }}>
+      <Text style={{ color: "white" }}>tela de login</Text>
+    </View>
+  );
+}
