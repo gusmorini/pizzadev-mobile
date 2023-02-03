369c925af4f96f1a5f8d8ef8329a9adfc0a4493e
Gustavo Morini <gustavommh@gmail.com>
2023-02-03 20:51:48 -0300

    useState email, pass

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles.modules";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleLogin = () => {
+    if (!email || !pass) {
+      return;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("../../assets/logo.png")} />
@@ -12,6 +22,8 @@ export default function SignIn() {
           placeholder="Digite seu e-mail"
           keyboardType="email-address"
           placeholderTextColor="#888"
+          value={email}
+          onChangeText={setEmail}
         />
 
         <TextInput
@@ -19,9 +31,11 @@ export default function SignIn() {
           placeholder="Digite sua senha"
           placeholderTextColor="#888"
           secureTextEntry={true}
+          value={pass}
+          onChangeText={setPass}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.textButton}>Acessar</Text>
         </TouchableOpacity>
       </View>
