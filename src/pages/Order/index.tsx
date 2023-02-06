76cc2a52ac40d7d66ec533c549696d9328f12640
Gustavo Morini <gustavommh@gmail.com>
2023-02-06 11:20:25 -0300

    useRoute params

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -2,10 +2,26 @@ import { useState } from "react";
 import { styles } from "./styles.modules";
 import { Text, TouchableOpacity, SafeAreaView, TextInput } from "react-native";
 
+import { useRoute, RouteProp } from "@react-navigation/native";
+
+type RouteDetailParams = {
+  Order: {
+    number: string | number;
+    order_id: string;
+  };
+};
+
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
+
 export default function Order() {
+  const route = useRoute<OrderRouteProps>();
+
+  const { number, order_id } = route.params;
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>order screen</Text>
+      <Text>Table: {number}</Text>
+      <Text>Id: {order_id}</Text>
     </SafeAreaView>
   );
 }
