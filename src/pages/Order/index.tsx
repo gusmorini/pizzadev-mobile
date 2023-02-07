469aa24ffb914ae1f3aa257a19a774ee96ebb710
Gustavo Morini <gustavommh@gmail.com>
2023-02-07 15:45:27 -0300

    screen order

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { styles } from "./styles.modules";
-import { Text, TouchableOpacity, SafeAreaView, TextInput } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+  View,
+} from "react-native";
 
 import { useRoute, RouteProp } from "@react-navigation/native";
+
+import { AntDesign } from "@expo/vector-icons";
 
 type RouteDetailParams = {
   Order: {
@@ -20,8 +28,41 @@ export default function Order() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Table: {number}</Text>
-      <Text>Id: {order_id}</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Mesa {number}</Text>
+        <TouchableOpacity>
+          <AntDesign name="delete" size={24} color="#ff3f4b" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity style={styles.input}>
+          <Text style={styles.inputText}>Pizzas</Text>
+          <AntDesign name="down" size={14} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.input}>
+          <Text style={styles.inputText}>Pizza de frango com catupiry</Text>
+          <AntDesign name="down" size={14} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.containerQtd}>
+        <Text style={styles.qtdText}>Quantidade</Text>
+        <TextInput
+          keyboardType="numeric"
+          style={[styles.input, styles.inputQtd]}
+        ></TextInput>
+      </View>
+
+      <View style={styles.actions}>
+        <TouchableOpacity style={[styles.input, styles.inputPlus]}>
+          <AntDesign name="plus" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.input, styles.inputNext]}>
+          <Text style={[styles.inputText, styles.inputTextNext]}>Avançar</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
--- src/pages/Order/styles.modules.ts
+++ src/pages/Order/styles.modules.ts
@@ -3,8 +3,73 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    backgroundColor: "#1d1d2e",
+    paddingHorizontal: 20,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 20,
+  },
+  headerText: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#101026",
+    color: "white",
+    borderRadius: 4,
+    height: 40,
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    flexDirection: "row",
+  },
+  inputText: {
+    color: "white",
+    fontSize: 12,
+  },
+  containerQtd: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  qtdText: {
+    color: "white",
+    fontWeight: "bold",
+    marginRight: 10,
+    fontSize: 20,
+  },
+  inputQtd: {
+    flex: 1,
+    textAlign: "center",
+    maxWidth: "40%",
+  },
+  actions: {
+    flexDirection: "row",
+  },
+  inputPlus: {
+    marginRight: 10,
+    backgroundColor: "#3fd1ff",
+    width: "25%",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#1d1d2e",
+    height: 40,
+  },
+  inputNext: {
+    flex: 1,
+    backgroundColor: "#3fffa3",
+    justifyContent: "center",
+  },
+  inputTextNext: {
+    fontSize: 14,
+    color: "#101026",
+    fontWeight: "bold",
   },
 });
